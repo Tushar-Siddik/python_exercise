02a459052697d1aeda2eabf8571c0b9c7e57bf50
Tushar-Siddik <s.r.toshar@gmail.com>
2023-01-18 23:43:20 +0600

    first 10 practice questions

---
+++ main.py
@@ -0,0 +1,39 @@
+# Exercise 01: Write a program to find the type of the variable name
+# Variable, name=”Hello there”, number=9876, list = [1, 'a'] etc.
+
+
+# Exercise 01: Write a program to find the length of the variable name
+# Variable, name=”Hello there”
+
+
+# Exercise 02: Write a program to find the type of the variable name
+# name=”Hello there”
+
+
+# Exercise 03: Write a function that takes 2 numbers as arguments (age of
+# two brothers) and find who is elder
+# Hints: Use condition inside the function
+
+
+# Exercise 04: Write a program to find the index of 7
+# numbers=[3, 5, 1, 9, 7, 2, 8 ]
+
+
+# Exercise 05: Write a program to sort the numbers in Ascending order
+# numbers=[3, 5, 1, 9, 7, 2, 8 ]
+
+
+# Exercise 06: Write a function named “isLandscape” that
+# takes 2 numbers (image width and height) as arguments 
+# and the function returns Landscape if image width has higher value than height. 
+# Returns Portrait otherwise
+# Hints: Use condition inside the function
+
+
+# Exercise 07: FizzBuzz Exercise
+# Write a function that takes 1 number as argument. 
+# The function should return “Fizz” if the number is divisible by 3, 
+# the function should return “Buzz” if the number is divisible by 5, 
+# the function should return “FizzBuzz” if the number is divisible by both 5 and 3,
+# otherwise return “Not a Fizz-buzz number”
+# Hints: Use condition inside the function
